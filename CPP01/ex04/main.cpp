ebab3be93c6bcde88c13d21590b1b387c0414eaa
korolyo <Korolyo@gmail.com>
2022-03-29 13:26:20 +0300

    CPP01ex04 done

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 #include <fstream>
 
+std::string	ft_replace(std::string line, int pos, int len, std::string s2)
+{
+	std::string	before;
+	std::string after;
+	std::string newline;
+
+	before = line.substr(0, pos);
+	after = line.substr(pos + len, line.length() - pos + len);
+	newline = before + s2 + after;
+	return (newline);
+}
+
 int main(int argc, char **argv)
 {
 	std::string	filename;
 	std::string	line;
 	std::string	s1 = argv[2];
-	std::string *s_buf = &s1;
 	std::string	s2 = argv[3];
 	int			pos;
 
@@ -16,16 +27,12 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 	filename = argv[1];
-
 	std::ifstream input(filename);
 	std::ofstream output(filename + ".replace");
 	while (std::getline(input, line))
 	{
-		if ((pos = line.find(*s_buf)) != std::string::npos)
-		{
-			*s_buf = s2;
-			std::cout << line << std::endl;
-		}
+		if ((pos = line.find(s1)) != std::string::npos)
+			line = ft_replace(line, pos, s1.length(), s2);
 		output << line << std::endl;
 	}
 	return 0;
