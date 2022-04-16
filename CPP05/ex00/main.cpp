15f3bb21d86966ff0b56a5da4a1944cea665ba67
korolyo <Korolyo@gmail.com>
2022-04-16 20:27:17 +0300

    Some fixes

--- CPP05/ex00/main.cpp
+++ CPP05/ex00/main.cpp
@@ -2,6 +2,40 @@
 
 int main( void ) {
 
+
+	std::cout << "--- 1. Trying to create a bur with sign grade 0 ---" << std::endl;
+		try
+		{
+			Bureaucrat a("a", 0);
+
+			std::cout << a;
+			std::cout << "if you can read this, there a mistake and final mark is 0" << std::endl;
+		}
+		catch(std::exception &ex2)
+		{
+			std::cout << "Exception caught: " << e.what() << std::endl;
+		}
+	std::cout << std::endl;
+
+	std::cout << "--- 2. Trying to create a form with sign grade 151 ---" << std::endl;
+		try
+		{
+			Bureaucrat a("a", 151);
+		
+			std::cout << a;
+			std::cout << "if you can read this, there a mistake and final mark is 0" << std::endl;
+		}
+		catch(std::exception &ex1)
+		{
+			std::cout << "Exception caught: " << e.what() << std::endl;
+		}
+	std::cout << std::endl;
+
+	std::cout << "--- 3. Trying to create a form and show it ---" << std::endl;
+	Form coolForm("Form from 3", 20, 25);
+	std::cout << coolForm;
+	std::cout << std::endl;
+
 	Bureaucrat	buro1;
 	Bureaucrat	buro2 = Bureaucrat("yo", 150);
 	Bureaucrat	buro3 = Bureaucrat("yo1", 1);
